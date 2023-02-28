4b72dc359ce499314d76a8bdc25b12df5fe40a0a
nguyenvanvinh10122002 <nguyenvanvinhisco22@gmail.com>
2023-02-28 21:08:11 +0700

    Add project files.

---
+++ ASM5/Controllers/BookController.cs
@@ -0,0 +1,123 @@
+﻿using ASM5.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASM5.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            var context = new BookModelContext();
+            return View(context.Books.ToList());
+        }
+        public ActionResult GetBookCategory(int id)
+        {
+            var context = new BookModelContext();
+            return View("Index", context.Books.Where(p => p.CategoryId == id).ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var context = new BookModelContext();
+            ViewBag.ListCategory = context.Categories.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Book newBook)
+        {
+            var context = new BookModelContext();
+
+            if(ModelState.IsValid)
+            {
+                context.Books.Add(newBook);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Create");
+        }
+
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var context = new BookModelContext();
+            Book findBook = context.Books.FirstOrDefault(p => p.Id== id);
+            if(findBook==null) {
+                return HttpNotFound("Sách không tồn tại...!");
+            }
+            return View(findBook);
+           
+        }
+        [HttpPost]
+        public ActionResult Delete(Book findBook)
+        {
+            var context = new BookModelContext();
+            Book deBook = context.Books.FirstOrDefault(p => p.Id == findBook.Id);
+            if (findBook == null)
+            {
+                return HttpNotFound("Sách không tồn tại...!");
+            }
+            context.Books.Remove(deBook);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var context = new BookModelContext();
+            Book findBook = context.Books.FirstOrDefault(p => p.Id == id);
+            ViewBag.ListCategory = context.Categories.ToList();
+            if (findBook == null)
+            {
+                return HttpNotFound("Sách không tồn tại...!");
+            }
+            return View(findBook);
+        }
+        [HttpPost]
+        public ActionResult Edit(Book editBook)
+        {
+            var context = new BookModelContext();
+            Book findBook = context.Books.FirstOrDefault(p => p.Id == editBook.Id);
+            if (findBook == null)
+            {
+                return HttpNotFound("Sách không tồn tại...!");
+            }
+
+            findBook.Author = editBook.Author;
+            findBook.Category = editBook.Category;
+            findBook.CategoryId = editBook.CategoryId;
+            findBook.Price= editBook.Price; 
+            findBook.Description = editBook.Description;
+            findBook.Title = editBook.Title;
+            findBook.Image= editBook.Image;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int id)
+        {
+            var context = new BookModelContext();
+            Book fristBook = context.Books.FirstOrDefault(p => p.Id == id);
+            if (fristBook == null)
+            {
+                return HttpNotFound("Sách không tồn tại...!");
+            }
+            return View(fristBook);
+        }
+
+        public ActionResult GetCategory()
+        {
+            var context = new BookModelContext();
+            var listCategory = context.Categories.ToList();
+            return PartialView(listCategory);
+        }
+    }
+
+}
---
+++ ASM5/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿namespace ASM5.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+    using System.Web;
+
+    [Table("Book")]
+    public partial class Book
+    {
+        public int Id { get; set; }
+
+        [Required (ErrorMessage ="Tựa sách không được để trống !")]
+        [StringLength(150)]
+        public string Title { get; set; }
+
+        [StringLength(150, ErrorMessage ="Tên tác giả có tối đa 150 kí tự")]
+        public string Author { get; set; }
+        [Range(1, 999, ErrorMessage = "Giá sách từ 1-999")]
+        public decimal? Price { get; set; }
+
+        public string Description { get; set; }
+
+        [StringLength(100)]
+        public string Image { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+        //public HttpPostedFileBase ImageFife { get; set; }
+    }
+}
